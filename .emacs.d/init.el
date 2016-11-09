bfdf77d1ed7f4be785d6bc0b42a0b48cf3843011
Thiam Lee <matheuristic@users.noreply.github.com>
2016-11-09 16:53:19 -0500

    minor edits to comments in Emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -81,7 +81,7 @@
     (set-frame-position this-frame (+ (car fpos) x) (+ (cdr fpos) y))))
 
 (defun my-move-frame-pct (x y)
-  "Move selected frame by X% and Y% of the display horizontally and vertically."
+  "Move selected frame within display by X% horizontally and Y% vertically."
   (my-move-frame (* x (/ (x-display-pixel-width) 100))
                  (* y (/ (x-display-pixel-height) 100))))
 
@@ -451,20 +451,20 @@
       "
 Projectile: %(projectile-project-root)
 
-Buffer  _bb_  : switch to buffer           _bi_  : ibuffer
-        _bk_  : kill buffers               _bo_  : switch buffer (other window)
+Buffer  _bb_  : switch to buffer          _bi_  : ibuffer
+        _bk_  : kill buffers              _bo_  : switch buffer (other window)
       
-File    _ff_  : find file                  _fw_  : find file dwim
-        _fd_  : find file in dir           _fp_  : find file in known projects
-        _fof_ : find file (other window)   _fow_ : find file dwim (other window)
+File    _ff_  : find file                 _fw_  : find file dwim
+        _fd_  : find file in dir          _fp_  : find file in known projects
+        _fof_ : find file (other window)  _fow_ : find file dwim (other window)
         _fr_  : recent files
 
-Dir     _dd_  : find dir                   _do_  : find dir (other window)
+Dir     _dd_  : find dir                  _do_  : find dir (other window)
 
-Search  _sg_  : grep                       _so_  : multi-occur
+Search  _sg_  : grep                      _so_  : multi-occur
 
-Cache   _cc_  : cache current file         _cC_  : clear cache
-        _cx_  : remove known project       _cX_  : cleanup known projects
+Cache   _cc_  : cache current file        _cC_  : clear cache
+        _cx_  : remove known project      _cX_  : cleanup known projects
 
 "
       ("bb"  projectile-switch-to-buffer)
