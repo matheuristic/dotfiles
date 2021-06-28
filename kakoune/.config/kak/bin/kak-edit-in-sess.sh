35d3918adcfe9f6707aae7ed36c69c57eb05facd
Thiam Lee <matheuristic@users.noreply.github.com>
2021-06-28 15:59:22 -0400

    add description and comments for kak-edit-in-sess.sh script

--- kakoune/.config/kak/bin/kak-edit-in-sess.sh
+++ kakoune/.config/kak/bin/kak-edit-in-sess.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Opens a file in a given Kakoune client and session
+# Requires realpath which is on Linux systems but which has to be installed on
+# MacOS either using MacPorts or Homebrew
+
 TGTCLIENT=$1
 TGTSESS=$2
 TGTFILE=$3
